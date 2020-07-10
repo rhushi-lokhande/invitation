7c9c6d21efe5ae2ea858674a48018f130ab4d334
Rhushikesh Lokhande <Rhushikesh_Lokhande@epam.com>
2020-07-10 13:24:36 +0530

    added auto scrolling

--- js/script.js
+++ js/script.js
@@ -34,21 +34,5 @@ breakScroll = false;
     initializeClock('07/13/2020 11:00:00');
     
 
-    function autoScroll() {
-        var height = document.getElementById("container").scrollHeight - window.innerHeight;
-        var scrollHeight = 0;
-        var interval = setInterval(() => {
-            if (breakScroll || (scrollHeight > height)) {
-                clearInterval(interval)
-            } else {
-
-                scrollHeight = (window.lastScrollTop || 0) + 4;
-                // document.getElementById("container").scrollTop = scrollHeight;
-                window.scrollTo(0, scrollHeight)
-
-            }
-        }, 30)
-    }
-
 })();
 
